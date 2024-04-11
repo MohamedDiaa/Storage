c7ce3f072a4a42f899d3345895544c1a30fcbf09
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2024-04-11 11:56:27 +0200

    fix

---
+++ Storage/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Storage.Models
+{
+	public class Product
+	{
+        
+        public int Id;
+        public string Name;
+        public int Price;
+        public DateTime Orderdate;
+        public string Category;
+        public string Shelf;
+        public int Count;
+        public string Description;
+
+        public Product(int Id, string Name, DateTime Orderdate, string Category, string Shelf, int Count, string Description)
+		{
+            this.Id = Id;
+            this.Name = Name;
+            this.Orderdate = Orderdate;
+            this.Category = Category;
+            this.Shelf = Shelf;
+            this.Count = Count;
+            this.Description = Description;
+        }
+	}
+}
+
